d1941b89d71654e180e7aed474c1cbe6b5ceeb97
Samuel Bretas <samuelbretas@gmail.com>
2026-01-03 21:19:30 +0000

    Refactoring logic for including the SSH interface in the domain/model.go layer.

--- internal/worker/logic.go
+++ internal/worker/logic.go
@@ -5,50 +5,60 @@ import(
 "fmt"
 "log"
 
+"servers_updater/internal/domain"
 "servers_updater/internal/db"
 "servers_updater/internal/notify"
 "servers_updater/internal/security"
 "servers_updater/internal/ssh"
 )
 
-func ProcessSingleServer(ctx context.Context, host string, database *db.BoltDB) error {
- client, err := ssh.Connect(host, database)
- if err != nil {
-  msg := fmt.Sprinf("Failed to connect to SSH.: %v, err")
-  notify.SendEmail(host, "Connection err", msg)
-  return err
- }
+func ProcessSingleServer(ctx context.Context, host string, database *db.BoltDB, client domain.SSHClient) error {
  defer client.Close()
 
- packages, err := ssh.GetUpgradablePackages(client)
+ log.Printf("[%s] Initiating audit...", host)
+
+ output, err := client.ExecuteCommand("sudo apt-get upgrade -s")
  if err != nil {
-  notify.SendEmail(host, "Dry-Run error", err.Error())
+  msg := fmt.Sprintf("failure in the dry run in %s: %v", host, err)
+  sendErrorNotification(host, database, msg)
   return err
  }
-
- if len(packages) == 0 {
-  log.Printf("[%s] System already updated.", host)
+ 
+ packages := ssh.ParseAptOutput(output)
+ if len(packages) = 0 {
+  log.Printf("[%s] OK, no packages to update.", host)
   return nil
  }
 
  for _, pkg := range packages {
-  isVulnerable, info := security.CheckOS(pkg.Name, pkg.Version)
+  isVulnerable, info := security.CheckOSV(pkg.Name, pkg.NewVersion)
   if isVulnerable {
-	  msg := fmt.Sprintf("Update aborted! The package %s (%s)  has risks: %s", pkg.Name, pkgVersion, info) 
-  
-   notify.SendEmail(host, "Security Risk", msg)
-   return fmt.Errorf("Vulnerability found in %s", pkg.Name)
+   msg := fmt.Sprintf("VULNERABILITY: %s (Version: %s) - Details: %s", pkg.Name, pkg.NewVersion, info)
+   sendVulnerabilityNotification(host, database, packages, msg)
+
+   return fmt.Errorf("Secutiry: %s is vulnerable", pkg.Name)
   }
  }
 
- err = ssh.RunFinalUpgrade(client)
- if err = != nil {
-  notify.SendEmail(host, "Package installation failed.", err.Error())
+ log.Printf("[%s] No risks found. Applying packages...",host)
+ _, err = client.ExecuteCommand("sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y")
+ if err != nil {
+  sendErrorNotification(host, database, "Upgrade error:"+err.Error())
   return err
- }
-
- log.Printf("[%s] Updated packages successful.")
+}
+ 
+ log.Printf("[%s] System updated successfully.", host)
  return nil
+}
+
+func sendErrorNotification(host string, database *db.BoltDB, msg string) {
+ cfg, _ := database.GetSMTPConfig()
+ notify.SendHTMLEmail(cfg, domain.EmailData{Host: host, Status: "Error", ErrorMessage: msg})
+}
+
+func sendVulnerabilityNotification(host string, database *db.BoltDB, pkgs []domain.Package, msg string) {
+ cfg, _ := database.GetSMTPConfig()
+ notify.SendHTMLEmail(cfg, domain.EmailData{Host: host, Status: "Vulnerability", Packages: pkgs, ErrorMessage: msg})
 }
 
 
