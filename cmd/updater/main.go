3dc44039def133b69f23f537cfd074bae23015c1
Samuel Bretas <samuelbretas@gmail.com>
2026-01-14 23:40:40 +0000

    Refactoring the main method and including a UI layer for user interaction.

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -4,25 +4,16 @@
    "context"
    "log"
    "servers_updater/internal/db"
-   "servers_updater/internal/worker"
+   "servers_updater/internal/ui"
 )
 
 func main() {
-
-  database, err := db.New("updater.db")
-  if err != nil {
-    log.Fatal("Failed to start DB: %v, err")	
-  }
-  defer database.Close()
-
-  
-  hosts, err := config.ParseSSHConfig()
-  if err != nil {
-   log.Fatal("Error reading ssh config file.: %v, err")
-  }
-
-  ctx := context.Background()
-  if err := worker.Run(ctx, hosts, database); err != nil {
-   log.Printf("Process completed with errors.: %v", err)
-  }
+ database, err := db.New("updater.db")
+ if err != nil {
+  log.Fatal("Failed to start DB: %v, err")	
  }
+ defer database.Close()
+
+ appMenu := ui.NewMenu(database)
+ appMenu.ShowMainMenu()
+}
