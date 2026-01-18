696f97df3b019d0727562947d55cc9f4ec2fc2f3
Samuel Bretas <samuelbretas@gmail.com>
2026-01-18 23:59:28 +0000

    Refactoring the notify/smtp layer so that it can receive a list of recipients.

--- internal/notify/smtp.go
+++ internal/notify/smtp.go
@@ -9,7 +9,11 @@ import (
  "time"
 )
 
-func SendHtmlEmail(cfg domain.SMTPConfig, data domain.EmailData) error {
+func SendHtmlEmail(cfg domain.SMTPConfig, recipients []string,   data domain.EmailData) error {
+ if len(recipients) == 0 {
+  return fmt.Errorf("No recipients provided")
+ } 
+
  now := time.Now().UTC()
  data.Date = &now
  
@@ -19,14 +23,18 @@ func SendHtmlEmail(cfg domain.SMTPConfig, data domain.EmailData) error {
   return err
  }
 
- addr := fmt.Sprintf("%s:%s", cfg.Server, cfg.Port)
- subject := fmt.Sprintf("Subject: [Updater] Report of %s\n", data.Host)
- mime := "MIME-version: 1.0;\nContent-Type: text/html: charset=\"UTF-8\"\n\n"
- msg := []byte(subject + mime + body.String())
- auth := smtp.PlainAuth("", cfg.User, cfg.Password, cfg.Server)
+ toHeader := strings.Join(recipients, ",")
+ addr := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
+
+ headers := fmt.Sprintf("From: %s\r\n", cfg.User)
+ headers += fmt.Sprintf("To: %s\r\n", toHeader)
+ headers += fmt.Sprintf("Subject: [Updater] Report" - %s\r\n, data.Host)
+ headers += "MIME-version: 1.0;\r\n"
+ headers += "Content-Type: text/html: charset=\"UTF-8\";\r\n"
+ headers += "\r\n"
+
+ msg := []byte(headers + body.String())
+ auth := smtp.PlainAuth("", cfg.User, cfg.Password, cfg.Host)
  
- return smtp.SendMail(addr, auth, cfg.User, []string{cfg.To}, msg)
-
-
-
+ return smtp.SendMail(addr, auth, cfg.User, recipients, msg)
 }
