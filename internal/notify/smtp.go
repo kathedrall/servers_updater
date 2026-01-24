fee0c2a0036ca59e9599c9b0deff003f5734eb8e
Samuel Bretas <samuelbretas@gmail.com>
2026-01-24 00:02:32 +0000

    Setting variables and including string parameters

--- internal/notify/smtp.go
+++ internal/notify/smtp.go
@@ -7,6 +7,7 @@ import (
  "fmt"
  "net/smtp"
  "time"
+ "strings"
 )
 
 func SendHtmlEmail(cfg domain.SMTPConfig, recipients []string,   data domain.EmailData) error {
@@ -23,12 +24,12 @@ func SendHtmlEmail(cfg domain.SMTPConfig, recipients []string,   data domain.Ema
   return err
  }
 
- toHeader := strings.Join(recipients, ",")
+ toHeader := strings.Join(recipients, " , ")
  addr := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
 
  headers := fmt.Sprintf("From: %s\r\n", cfg.User)
  headers += fmt.Sprintf("To: %s\r\n", toHeader)
- headers += fmt.Sprintf("Subject: [Updater] Report" - %s\r\n, data.Host)
+ headers += fmt.Sprintf("Subject: [Updater] Report - %s\r\n", data.Host)
  headers += "MIME-version: 1.0;\r\n"
  headers += "Content-Type: text/html: charset=\"UTF-8\";\r\n"
  headers += "\r\n"
