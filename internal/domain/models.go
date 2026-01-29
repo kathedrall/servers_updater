20a2b9a78c0cee80ff432a3ffdc74d70c8a6ebe0
Samuel Bretas <samuelbretas@gmail.com>
2026-01-29 23:56:46 +0000

    refactor(Machine) include variables for discovery the os system in machine

--- internal/domain/models.go
+++ internal/domain/models.go
@@ -3,38 +3,45 @@ package domain
 import "time"
 
 type Machine struct {
- Id string `json:"id"`
- Host string `json:"host"`
- User string `json:"user"`
- Port int `json:"port"`
- Password string `json:"password"`
- KeyPath string `json:"key_path"`
- IsVulnerable bool `json:"is_vulnerable"`
+	Id           string `json:"id"`
+	Host         string `json:"host"`
+	User         string `json:"user"`
+	Port         int    `json:"port"`
+	Password     string `json:"password"`
+	KeyPath      string `json:"key_path"`
+	IsVulnerable bool   `json:"is_vulnerable"`
+
+	OSName      string    `json:"os_name"`
+	OSVersion   string    `json:"os_version"`
+	PrettyName  string    `json:"pretty_name"`
+	IsSupported bool      `json:"is_supported"`
+	Status      string    `json:"status"`
+	LastSeen    time.Time `json:"last_seen"`
 }
 
 type Package struct {
- Name string `json:"name"`
- CurrentVersion string `json:"current_version"`
- NewVersion string `json:"new_version"`
- CVEs []string `json:"cves"`
+	Name           string   `json:"name"`
+	CurrentVersion string   `json:"current_version"`
+	NewVersion     string   `json:"new_version"`
+	CVEs           []string `json:"cves"`
 }
 
 type SMTPConfig struct {
- Host     string
- Port     string
- User     string
- Password string
+	Host     string
+	Port     string
+	User     string
+	Password string
 }
 
 type EmailData struct {
- Host string
- Status string
- Packages []Package
- ErrorMessage string
- Date *time.Time
+	Host         string
+	Status       string
+	Packages     []Package
+	ErrorMessage string
+	Date         *time.Time
 }
 
 type SSHClient interface {
- ExecuteCommand(cmd string) (string, error)
- Close() error
+	ExecuteCommand(cmd string) (string, error)
+	Close() error
 }
